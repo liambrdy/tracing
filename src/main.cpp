72ea129af3e87d3abbaf23c3d3ade8e1cd2abbdc
Liam Brady <liam.james.brady@gmail.com>
2021-04-13 14:08:33 +0000

    correct image output

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,34 @@
 
 struct Pixel
 {
-    uint32_t r, g, b, a;
+    uint8_t r, g, b, a;
 };
 
 int main()
 {
-    std::vector<Pixel> pixels(256 * 256);
+    constexpr int32_t width = 256, height = 256;
 
-    for (uint32_t y = 0; y < 256; ++y)
+    std::vector<Pixel> pixels;
+
+    for (int32_t y = height - 1; y >= 0; --y)
     {
-        for (uint32_t x = 0; x < 256; ++x)
+        for (int32_t x = 0; x < width; ++x)
         {
             float r = (float)x / 256.0f;
             float g = (float)y / 256.0f;
             float b = 0.0f;
 
-            uint32_t ir = (uint32_t)(255.0f * r);
-            uint32_t ig = (uint32_t)(255.0f * g);
-            uint32_t ib = (uint32_t)(255.0f * b);
+            Pixel p = {};
+            p.r = (uint8_t)(255.0f * r);
+            p.g = (uint8_t)(255.0f * g);
+            p.b = (uint8_t)(255.0f * b);
+            p.a = 255;
 
-            pixels[y * 256 + x] = { ir, ig, ib, 255 };
+            pixels.push_back(p);
         }
     }
 
-    stbi_write_png("../output.png", 256, 256, 4, pixels.data(), 256 * 4);
+    stbi_write_png("../output.png", width, height, 4, pixels.data(), width * 4);
     
     return 0;
 }
